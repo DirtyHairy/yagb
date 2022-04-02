20778cf9475fc8797acec8bfd17bd07a6ae0bf8d
Christian Speckner <christian.speckner@mayflower.de>
2022-04-02 21:00:11 +0200

    Created.

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: 'public', to: '.' }],
+        }),
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
+};
