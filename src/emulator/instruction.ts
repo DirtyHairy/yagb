25de5169adb32bd2801cb602980f72c1b21a74d7
Sven Pöche <sven.poeche@mayflower.de>
2022-04-12 21:54:17 +0200

    OR d8

--- src/emulator/instruction.ts
+++ src/emulator/instruction.ts
@@ -262,6 +262,7 @@ apply(0xae, { op: Operation.xor, par1: r16.hl, mode1: AddressingMode.reg16ind8, 
 apply(0xb6, { op: Operation.or, par1: r16.hl, mode1: AddressingMode.reg16ind8, cycles: 2, len: 1 });
 
 apply(0xe6, { op: Operation.and, mode1: AddressingMode.imm8, cycles: 2, len: 2 });
+apply(0xee, { op: Operation.xor, mode1: AddressingMode.imm8, cycles: 2, len: 2 });
 
 // 0x01, 0x11, 0x21, 0x31
 [r16.bc, r16.de, r16.hl, r16.sp].forEach((reg, i) => {
