daf5a5d3b471eff72b302aeb549dccb988b4231a
Sven Pöche <sven.poeche@mayflower.de>
2022-04-13 01:33:45 +0200

    adjust github workflow

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,21 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: [
-      './spec/',
-  ],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['./spec/'],
+    coveragePathIgnorePatterns: ['./spec/'],
+    reporters: [
+        'default',
+        [
+            './node_modules/jest-html-reporter',
+            {
+                pageTitle: 'Test Report',
+                outputPath: 'test_results/jest-report.html',
+                includeFailureMsg: true,
+                includeSuiteFailure: true,
+                includeConsoleLog: true,
+                includeObsoleteSnapshots: true,
+            },
+        ],
+    ],
 };
