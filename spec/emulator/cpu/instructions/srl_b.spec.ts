053c845daca29611eadb59e63ac5d3032f9dd19f
Sven Pöche <sven.poeche@mayflower.de>
2022-04-15 17:08:53 +0200

    cb srl opcodes

---
+++ spec/emulator/cpu/instructions/srl_b.spec.ts
@@ -0,0 +1,64 @@
+import { Environment, newEnvironment } from '../../../support/_helper';
+import { flag, r16, r8 } from '../../../../src/emulator/cpu';
+
+describe('The glorious CPU', () => {
+    describe('SRL B', () => {
+        function setup(value: number, carry: boolean): Environment {
+            const env = newEnvironment([0xcb, 0x38]);
+
+            env.cpu.state.r8[r8.b] = value;
+
+            env.cpu.state.r8[r8.f] = flag.z | flag.n | flag.h | (carry ? flag.c : 0);
+
+            return env;
+        }
+
+        it('Shift B right', () => {
+            const { cpu } = setup(0xa5, true);
+
+            cpu.step(1);
+
+            expect(cpu.state.r8[r8.b]).toBe(0x52);
+        });
+
+        it('sets Z if zero', () => {
+            const { cpu } = setup(0x01, true);
+
+            cpu.step(1);
+
+            expect(cpu.state.r8[r8.f] & flag.z).toBe(flag.z);
+        });
+
+        it('does not set Z if not zero', () => {
+            const { cpu } = setup(0xa5, true);
+
+            cpu.step(1);
+
+            expect(cpu.state.r8[r8.f] & flag.z).toBe(0);
+        });
+
+        it('clears N, H', () => {
+            const { cpu } = setup(0xa5, true);
+
+            cpu.step(1);
+
+            expect(cpu.state.r8[r8.f] & flag.n & flag.h).toBe(0);
+        });
+
+        it('sets C if bit 0', () => {
+            const { cpu } = setup(0xa5, true);
+
+            cpu.step(1);
+
+            expect(cpu.state.r8[r8.f] & flag.c).toBe(flag.c);
+        });
+
+        it('does not set C if bit 0 is zero', () => {
+            const { cpu } = setup(0xaa, true);
+
+            cpu.step(1);
+
+            expect(cpu.state.r8[r8.f] & flag.c).toBe(0);
+        });
+    });
+});
