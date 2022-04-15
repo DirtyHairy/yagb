42036ea496c519c6aecd7e9c4ecc2f7b7c72da07
Christian Speckner <christian.speckner@mayflower.de>
2022-04-15 21:54:13 +0200

    Remove stray prettier-ignore

--- src/emulator/ppu.ts
+++ src/emulator/ppu.ts
@@ -95,7 +95,6 @@ export class Ppu {
     }
 
     printState(): string {
-        // prettier-ignore
         return `scanline=${this.scanline} mode=${this.mode} clockInMode=${this.clockInMode} frame=${this.frame} lcdc=${hex8(this.reg[reg.lcdc])} dma=${
             this.dmaInProgress ? 1 : 0
         } dmaCycle=${this.dmaCycle}`;
