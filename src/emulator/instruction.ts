e81d680de9441fe93adbb68edd1fe2daf894f176
Sven Pöche <sven.poeche@mayflower.de>
2022-04-16 20:40:10 +0200

    add instructions for not supported opcodes

--- src/emulator/instruction.ts
+++ src/emulator/instruction.ts
@@ -363,17 +363,17 @@ for (let i = 0; i < 0x200; i++)
         condition: Condition.always,
     };
 
-apply(0xd3, { op: Operation.none, cycles: 0, len: 0 });
-apply(0xdb, { op: Operation.none, cycles: 0, len: 0 });
-apply(0xdd, { op: Operation.none, cycles: 0, len: 0 });
-apply(0xe3, { op: Operation.none, cycles: 0, len: 0 });
-apply(0xe4, { op: Operation.none, cycles: 0, len: 0 });
-apply(0xeb, { op: Operation.none, cycles: 0, len: 0 });
-apply(0xec, { op: Operation.none, cycles: 0, len: 0 });
-apply(0xed, { op: Operation.none, cycles: 0, len: 0 });
-apply(0xf4, { op: Operation.none, cycles: 0, len: 0 });
-apply(0xfc, { op: Operation.none, cycles: 0, len: 0 });
-apply(0xfd, { op: Operation.none, cycles: 0, len: 0 });
+apply(0xd3, { op: Operation.none, cycles: 1, len: 1 });
+apply(0xdb, { op: Operation.none, cycles: 1, len: 1 });
+apply(0xdd, { op: Operation.none, cycles: 1, len: 1 });
+apply(0xe3, { op: Operation.none, cycles: 1, len: 1 });
+apply(0xe4, { op: Operation.none, cycles: 1, len: 1 });
+apply(0xeb, { op: Operation.none, cycles: 1, len: 1 });
+apply(0xec, { op: Operation.none, cycles: 1, len: 1 });
+apply(0xed, { op: Operation.none, cycles: 1, len: 1 });
+apply(0xf4, { op: Operation.none, cycles: 1, len: 1 });
+apply(0xfc, { op: Operation.none, cycles: 1, len: 1 });
+apply(0xfd, { op: Operation.none, cycles: 1, len: 1 });
 
 apply(0, { op: Operation.nop, cycles: 1, len: 1 });
 apply(0x10, { op: Operation.stop, cycles: 1, len: 1 });
