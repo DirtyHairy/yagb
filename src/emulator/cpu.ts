c3653c070fba2c24a26d47d482b7cbc809a43d97
Sven Pöche <sven.poeche@mayflower.de>
2022-04-18 21:09:45 +0200

    CCF

--- src/emulator/cpu.ts
+++ src/emulator/cpu.ts
@@ -463,9 +463,9 @@ export class Cpu {
 
         let operand = this.state.r8[r8.a];
 
-        const flagN = this.state.r8[r8.f] & flag.n,
-            flagC = this.state.r8[r8.f] & flag.c,
-            flagH = this.state.r8[r8.f] & flag.h;
+                const flagN = this.state.r8[r8.f] & flag.n,
+                    flagC = this.state.r8[r8.f] & flag.c,
+                    flagH = this.state.r8[r8.f] & flag.h;
 
         let setFlagC = 0;
         if (flagH || (!flagN && (operand & 0xf) > 0x09)) {
