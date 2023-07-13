1aa9957f6f98f711c68e4b1c8effb7837be1e762
Sven Pöche <sven.poeche@mayflower.de>
2023-07-14 01:25:19 +0200

    add support for cgb wram bank switching

--- yagb-core/src/emulator/ram.ts
+++ yagb-core/src/emulator/ram.ts
@@ -1,18 +1,54 @@
 import { Bus, ReadHandler, WriteHandler } from './bus';
 
+import { Mode } from "./mode";
 import { Savestate } from './savestate';
+
+export const enum reg {
+    svbk = 0xff70,
+}
+
+export const WRAMBankSize = 0x1000;
+
+interface Mapping {
+    wramBank0: Uint8Array;
+    wramBank1: Uint8Array;
+
+    wramBank0Index: number;
+    wramBank1Index: number;
+}
 
 const SAVESTATE_VERSION = 0x00;
 
 export class Ram {
+    constructor(private mode: Mode) {
+        this.configurations = new Array(0x8);
+        this.wram = new Uint8Array(this.wramSize());
+
+        if (this.mode === Mode.cgb) {
+            this.svbk = 1
+        }
+
+        this.initializeConfigurations();
+        this.updateBanks()
+    }
+
     install(bus: Bus): void {
-        for (let i = 0xc000; i < 0xfe00; i++) {
-            bus.map(i, this.wramRead, this.wramWrite);
+        for (let i = 0xc000; i < 0xd000; i++) {
+            bus.map(i, this.wramBank0Read, this.wramBank0Write);
+        }
+
+        if (this.mode === Mode.cgb) {
+            for (let i = 0xd000; i < 0xe000; i++) {
+                bus.map(i, this.wramBank0Read, this.wramBank0Write);
+            }
+
+            bus.map(reg.svbk, this.svbkRead, this.svbkWrite);
         }
 
         for (let i = 0xff80; i < 0xffff; i++) {
             bus.map(i, this.hiramRead, this.hiramWrite);
         }
+
     }
 
     reset(): void {
@@ -31,12 +67,74 @@ export class Ram {
         this.hiram.set(savestate.readBuffer(this.hiram.length));
     }
 
-    private wramRead: ReadHandler = (address) => this.wram[address & 0x1fff];
-    private wramWrite: WriteHandler = (address, value) => (this.wram[address & 0x1fff] = value);
+    private initializeConfigurations() {
+        const wramSize = (this.wramSize() / 1024) | 0;
+        const wramBanks = Math.max((wramSize / 8) | 0, 1);
+
+        const ramSlices = new Array(wramBanks);
+        for (let i = 0; i < wramBanks; i++) ramSlices[i] = this.wram.subarray(i * WRAMBankSize, (i + 1) * WRAMBankSize);
+
+        for (let i = 0x01; i < 0x08; i++) {
+            const wramBank0Index = 0;
+            const wramBank1Index = i;
+
+            this.configurations[i] = {
+                wramBank0: ramSlices[wramBank0Index],
+                wramBank1: ramSlices[wramBank1Index],
+                wramBank0Index: wramBank0Index,
+                wramBank1Index: wramBank1Index,
+            };
+        }
+    }
+
+    private updateBanks(): void {
+        const configuration = this.configurations[this.svbk];
+
+        this.wramBank0 = configuration.wramBank0;
+        this.wramBank1 = configuration.wramBank1;
+
+        this.wramBank0Index = configuration.wramBank0Index;
+        this.wramBank1Index = configuration.wramBank1Index;
+    }
+
+    private wramSize(): number {
+        return this.mode === Mode.dmg ? 4 * 1024 : 32 * 1024;
+    }
+
+    private svbkRead: ReadHandler = () => this.svbk;
+    private svbkWrite: WriteHandler = (_, value) => {
+        value = value & 0x07;
+
+        if (value === 0x00) {
+            value = 0x01;
+        }
+
+        this.svbk = value;
+
+        this.updateBanks();
+    };
+
+
+    private wramBank0Read: ReadHandler = (address) => this.wramBank0[address & 0x1fff];
+    private wramBank0Write: WriteHandler = (address, value) => (this.wramBank0[address & 0x1fff] = value);
+
+    private wramBank1Read: ReadHandler = (address) => this.wramBank1[address & 0x1fff];
+    private wramBank1Write: WriteHandler = (address, value) => (this.wramBank1[address & 0x1fff] = value);
 
     private hiramRead: ReadHandler = (address) => this.hiram[address & 0x7f];
     private hiramWrite: WriteHandler = (address, value) => (this.hiram[address & 0x7f] = value);
 
-    private wram = new Uint8Array(0x2000);
+    private configurations: Array<Mapping>;
+
+    private wramBank0!: Uint8Array;
+    private wramBank1!: Uint8Array;
+
+    private wramBank0Index = 0;
+    private wramBank1Index = 0;
+
+    private svbk = 1;
+
+    private wram: Uint8Array;
     private hiram = new Uint8Array(0x7f);
+
 }
