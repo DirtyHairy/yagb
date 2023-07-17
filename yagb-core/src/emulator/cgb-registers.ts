c05f85580e2ad856a33473f8eafc9bbd055d2e77
Sven Pöche <sven.poeche@mayflower.de>
2023-07-17 15:28:12 +0200

    add support for cgb registers ocps/obpi (0xff68) and ocpd/obpd (0xff6b)

--- yagb-core/src/emulator/cgb-registers.ts
+++ yagb-core/src/emulator/cgb-registers.ts
@@ -4,6 +4,8 @@ export const enum cgbRegisters {
     rp = 0xff56,
     bgpi = 0xff68,
     bgpd = 0xff69,
+    obpi = 0xff6a,
+    obpd = 0xff6b,
 }
 
 export function isCgbRegister(address: number): boolean {
@@ -13,6 +15,8 @@ export function isCgbRegister(address: number): boolean {
         case cgbRegisters.rp:
         case cgbRegisters.bgpi:
         case cgbRegisters.bgpd:
+        case cgbRegisters.obpi:
+        case cgbRegisters.obpd:
             return true;
 
         default:
