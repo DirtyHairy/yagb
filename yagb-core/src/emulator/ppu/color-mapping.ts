19bff56d5cba5c97e89d15f8e5bd005cc71dc758
Christian Speckner <christian.speckner@mayflower.de>
2023-07-26 14:33:54 +0200

    Port  color correction from sameboy.

--- yagb-core/src/emulator/ppu/color-mapping.ts
+++ yagb-core/src/emulator/ppu/color-mapping.ts
@@ -1,21 +1,19 @@
-export const COLOR_MAPPING = createPaletteMapping();
+export const COLOR_MAPPING = new Uint32Array(0x8000);
+
+const CHANNEL_LOOKUP = new Uint8Array([
+    0, 6, 12, 20, 28, 36, 45, 56, 66, 76, 88, 100, 113, 125, 137, 149, 161, 172, 182, 192, 202, 210, 218, 225, 232, 238, 243, 247, 250, 252, 254, 255,
+]);
+
+const GAMMA = 2.2;
 
 function convert(rgb555: number): number {
-    let r = rgb555 & 0x1f;
-    let g = (rgb555 >>> 5) & 0x1f;
-    let b = (rgb555 >>> 10) & 0x1f;
+    const r = CHANNEL_LOOKUP[rgb555 & 0x1f];
+    let g = CHANNEL_LOOKUP[(rgb555 >>> 5) & 0x1f];
+    const b = CHANNEL_LOOKUP[(rgb555 >>> 10) & 0x1f];
 
-    r = (r << 3) | (r >>> 2);
-    g = (g << 3) | (g >>> 2);
-    b = (b << 3) | (b >>> 2);
+    if (g !== b) g = Math.round(Math.pow((Math.pow(g / 255, GAMMA) * 3 + Math.pow(b / 255, GAMMA)) / 4, 1 / GAMMA) * 255);
 
     return 0xff000000 | (b << 16) | (g << 8) | r;
 }
 
-function createPaletteMapping(): Uint32Array {
-    const mapping = new Uint32Array(0x8000);
-
-    for (let rgb555 = 0; rgb555 < 0x8000; rgb555++) mapping[rgb555] = convert(rgb555);
-
-    return mapping;
-}
+for (let rgb555 = 0; rgb555 < 0x8000; rgb555++) COLOR_MAPPING[rgb555] = convert(rgb555);
