3c44eb5bedad5de8d80e1103d4653e7272588cfd
Christian Speckner <christian.speckner@mayflower.de>
2023-08-08 17:59:13 +0200

    Fix joypad interrupts. How could this go unnoticed...

--- yagb-core/src/emulator/joypad.ts
+++ yagb-core/src/emulator/joypad.ts
@@ -49,7 +49,7 @@ export class Joypad {
         this.keys[k] = 1;
         const after = this.joypadRead(0xff00) & 0x0f;
 
-        if (!!before && !after) this.interrupt.raise(irq.joypad);
+        if (before & ~after) this.interrupt.raise(irq.joypad);
     }
 
     up(k: key): void {
@@ -63,7 +63,7 @@ export class Joypad {
         this.keys.fill(0);
         const after = this.joypadRead(0xff00) & 0x0f;
 
-        if (!!before && !after) this.interrupt.raise(irq.joypad);
+        if (before & ~after) this.interrupt.raise(irq.joypad);
     }
 
     private joypadRead: ReadHandler = () => {
@@ -91,7 +91,7 @@ export class Joypad {
         this.joypad = value & 0xff;
         const after = this.joypadRead(0xff00) & 0x0f;
 
-        if (!!before && !after) this.interrupt.raise(irq.joypad);
+        if (before && ~after) this.interrupt.raise(irq.joypad);
     };
 
     private joypad = 0x00;
